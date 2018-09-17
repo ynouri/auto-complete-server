2081a5706d4724ef3dea24d0756ff71e9f168ea9
ynouri <ynouri@users.noreply.github.com>
2018-09-16 20:46:25 -0400

    Implement MPC build trie with unit tests

--- auto_complete_server/models/mpc.py
+++ auto_complete_server/models/mpc.py
@@ -2,6 +2,8 @@ from .base import BaseAutoCompleteModel
 import datrie
 import string
 import pandas as pd
+import json
+import nltk
 
 
 class MostPopularCompletionModel(BaseAutoCompleteModel):
@@ -14,7 +16,57 @@ class MostPopularCompletionModel(BaseAutoCompleteModel):
 
     def build_trie(self, corpus_file):
         """Read a corpus file and build the trie data structure."""
+        # Deserialize the corpus file into a dict
+        with open(corpus_file, encoding='utf8') as f:
+            corpus_json = json.loads(f.read())
+
+        # Create a dataframe of messages from the corpus dict
+        df_messages = pd.io.json.json_normalize(
+            data=corpus_json,
+            record_path=['Issues', 'Messages'],
+            meta=[
+                ['Issues', 'CompanyGroupId'],
+                ['Issues', 'IssueId']
+            ]
+        )
+
+        # Rename columns for simplicity of usage
+        new_columns = {
+            'Issues.IssueId': 'IssueId',
+            'Issues.CompanyGroupId': 'CompanyGroupId'
+        }
+        df_messages.rename(columns=new_columns, inplace=True)
+
+        # Instantiate a Punkt tokenizer from the english pickle
+        tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
+
+        # Split each message row into its different sentences and create a
+        # dataframe of sentences. Note: the creation of the sentences dataframe
+        # might not be scalable for a corpus of a billion messages with this
+        # implementation. A clever usage of pandas stack() and joins might
+        # enable to do it in a more efficient fashion.
+        columns = ['IssueId', 'CompanyGroupId', 'IsFromCustomer', 'SentenceId',
+                   'Sentence']
+        df_sentences = pd.DataFrame(columns=columns)
+        for message in df_messages.itertuples():
+            sentences = tokenizer.tokenize(message.Text)
+            for i, sentence in enumerate(sentences):
+                new_row = [message.IssueId, message.CompanyGroupId,
+                           message.IsFromCustomer, i, sentence]
+                df_sentences = df_sentences.append(
+                    dict(zip(columns, new_row)),
+                    ignore_index=True
+                )
+
+        # Computes the frequencies for each non-customer sentence in the corpus
+        not_customer = df_sentences.IsFromCustomer == False  # noqa: E712
+        sentences_freq = df_sentences[not_customer].Sentence.value_counts()
+
+        # Create and build the trie by inserting each sentence and its freq
         self.trie = datrie.Trie(string.printable)
+        for sentence, freq in sentences_freq.iteritems():
+            self.trie[sentence] = freq
+        return self.trie
 
     def generate_completions(self, prefix):
         """Generate completions for a given prefix."""
--- tests/models/test_mpc.py
+++ tests/models/test_mpc.py
@@ -1,7 +1,39 @@
+import pytest
 from auto_complete_server.models.mpc import MostPopularCompletionModel
+import os
+
+TEST_CORPUS_FILE = os.path.join('data', 'test_conversations.json')
 
 
 def test_model_init():
     """Test the MPC model init."""
     mpc = MostPopularCompletionModel()
     assert mpc
+
+
+@pytest.fixture(scope='module')
+def mpc():
+    """Set up a MPC model and build trie from a test corpus file."""
+    mpc = MostPopularCompletionModel()
+    mpc.build_trie(TEST_CORPUS_FILE)
+    yield mpc
+
+
+def test_trie_is_built(mpc):
+    """Test if the trie exists."""
+    assert mpc.trie
+
+
+def test_trie_keys(mpc):
+    """Test that a key is the trie."""
+    test_key = 'I am sorry for the inconveniences.'
+    test_keys = mpc.trie.keys('I')
+    assert test_key in test_keys
+
+
+def test_trie_value(mpc):
+    """Test that a sentence frequency is correctly returned by the trie."""
+    test_key = 'How can I help you?'
+    test_value = mpc.trie[test_key]
+    expected_value = 4
+    assert test_value == expected_value
